9bb1bb14ff7aff27126183bc39520261e84edb7c
Matteo Carlo Rossi <31008136+RoninCreativeStudio@users.noreply.github.com>
2023-02-04 19:00:34 +0100

    fuel system

--- Assets/_Project/Scripts/SpaceshipControls/RocketControls.cs
+++ Assets/_Project/Scripts/SpaceshipControls/RocketControls.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketControls : MonoBehaviour
 {
@@ -8,7 +9,12 @@ public class RocketControls : MonoBehaviour
     public Vector3 Velocity = Vector3.zero;
     private bool m_RocketLaunched = false;
     private float m_InitialCharge = 0.0f;
-
+    [Header("Fuel")]
+    public float Fuel = 1.0f;
+    public float FuelCumsuptionSpeed;
+    public float RotationSpeed = 1;
+    public float BoostSpeed = 1;
+    [Header("Charge")]
     float m_StartTime;
     public float Duration = 5.0f;
     private bool m_StopIntialCharge;
@@ -18,6 +24,9 @@ public class RocketControls : MonoBehaviour
     public MeshRenderer RightMat;
     public MeshRenderer LeftMat;
     public MeshRenderer BottomMat;
+
+    [Header("UI")]
+    public Slider FuelSlider;
 
     // Start is called before the first frame update
     void Start()
@@ -37,21 +46,57 @@ public class RocketControls : MonoBehaviour
             {
                 ApplyChargeToVelocity();
             }
+            else
+            {
+                if(Fuel > 0.0f)
+                {
+                    if (Input.GetKey(KeyCode.A)) // ROTATE
+                    {
+                        transform.Rotate(Vector3.forward * Time.deltaTime * RotationSpeed);
+                        ConsumeFuel();
+                    }
+                    else if(Input.GetKey(KeyCode.D)) // ROTATE
+                    {
+                        transform.Rotate(Vector3.back * Time.deltaTime * RotationSpeed);
+                        ConsumeFuel();
+                    }
+
+                    if (Input.GetKey(KeyCode.W)) // BOOST FORWARD
+                    {
+                        Velocity += transform.TransformDirection(Vector3.right) * Time.deltaTime * BoostSpeed;
+                        ConsumeFuel();
+                    }
+                    else if (Input.GetKey(KeyCode.S)) // BOOST BACKWARD
+                    {
+                        Velocity -= transform.TransformDirection(Vector3.right) * Time.deltaTime * BoostSpeed;
+                        ConsumeFuel();
+                    }
+                }
+
+                FuelSlider.value = Fuel;
+            }
             transform.position += Velocity * Time.deltaTime;
         }
+    }
+
+    private void ConsumeFuel()
+    {
+        Fuel -= FuelCumsuptionSpeed * Time.deltaTime;
+        Fuel = Mathf.Clamp01(Fuel);
     }
 
     public IEnumerator StopChargedVelocity()
     {
         yield return new WaitForSeconds(Duration);
         m_StopIntialCharge = true;
+        Debug.Log("Here");
     }
 
     private void ApplyChargeToVelocity()
     {
         m_StartTime += Duration * Time.deltaTime;
         float verticalSpeed = Mathf.SmoothStep(Velocity.z, m_InitialCharge, m_StartTime);
-        Velocity = transform.TransformDirection(new Vector3(0,0, verticalSpeed)) / Mass;      
+        Velocity = transform.TransformDirection(new Vector3(verticalSpeed, 0, 0)) / Mass;    
         
     }
 
